b6b9f4f4c1da52d304c5c6e9671c671abd673763
martim1705 <up202304944@up.pt>
2026-04-30 16:08:38 +0100

    added the buildWebs funct. Not done, does not check intersection. Next commit, the function that checks intersections will be implemented

--- include/Web.h
+++ include/Web.h
@@ -1,7 +1,8 @@
 #ifndef PROJETO2_WEB_H
 #define PROJETO2_WEB_H
-#include <Structures.h>
+#include "Structures.h"
 
+std::vector<Web> buildWebs(const std::vector<LiveRange>& ranges);
 
-
+bool intersects(const Web& web, const LiveRange& range);
 #endif //PROJETO2_WEB_H
--- src/Web.cpp
+++ src/Web.cpp
@@ -1,3 +1,43 @@
-//
-// Created by leme on 29/04/2026.
-//
+// build web
+// For each variable:
+//      Joins ranges that intersect each other
+//      Creates a web for each group
+#include "Web.h"
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+
+std::vector<Web> buildWebs(const std::vector<LiveRange>& ranges) {
+    std::unordered_map<std::string, std::vector<LiveRange>> group;
+
+    for ( const auto& range : ranges) {
+        group[range.variable].push_back(range);
+    }
+
+    std::vector<Web> webs;
+    int nextWebId = 0;
+
+    for (auto& [var, varRanges] : group) {
+
+        std::vector<bool> used(varRanges.size(), false);
+
+        for (size_t i = 0; i < varRanges.size(); i++) {
+            if (used[i]) continue;
+
+            Web web;
+            web.id = nextWebId++;
+            web.variable = var;
+            web.points = varRanges[i].points;
+
+            used[i] = true;
+            webs.push_back(web);
+        }
+    }
+
+    return webs;
+}
+
+
+bool intersects(const Web& web, const LiveRange& range) {
+    return true;
+}
