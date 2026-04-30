3027f0ed4942446985458368b757ce456642fabb
martim1705 <up202304944@up.pt>
2026-04-30 16:24:05 +0100

    added the logic to create webs

--- include/Web.h
+++ include/Web.h
@@ -4,5 +4,9 @@
 
 std::vector<Web> buildWebs(const std::vector<LiveRange>& ranges);
 
+bool intersects(const LiveRange& a, const LiveRange& b);
+
+bool sameLine(const ProgramPoint& a, const ProgramPoint& b);
+
 bool intersects(const Web& web, const LiveRange& range);
 #endif //PROJETO2_WEB_H
--- src/Web.cpp
+++ src/Web.cpp
@@ -10,7 +10,7 @@
 std::vector<Web> buildWebs(const std::vector<LiveRange>& ranges) {
     std::unordered_map<std::string, std::vector<LiveRange>> group;
 
-    for ( const auto& range : ranges) {
+    for (const auto& range : ranges) {
         group[range.variable].push_back(range);
     }
 
@@ -18,7 +18,6 @@ std::vector<Web> buildWebs(const std::vector<LiveRange>& ranges) {
     int nextWebId = 0;
 
     for (auto& [var, varRanges] : group) {
-
         std::vector<bool> used(varRanges.size(), false);
 
         for (size_t i = 0; i < varRanges.size(); i++) {
@@ -30,6 +29,26 @@ std::vector<Web> buildWebs(const std::vector<LiveRange>& ranges) {
             web.points = varRanges[i].points;
 
             used[i] = true;
+
+            bool changed = true;
+
+            while (changed) {
+                changed = false;
+
+                for (size_t j = 0; j < varRanges.size(); j++) {
+                    if (used[j]) continue;
+
+                    if (intersects(web, varRanges[j])) {
+                        for (const ProgramPoint& p : varRanges[j].points) {
+                            web.points.push_back(p);
+                        }
+
+                        used[j] = true;
+                        changed = true;
+                    }
+                }
+            }
+
             webs.push_back(web);
         }
     }
@@ -37,7 +56,30 @@ std::vector<Web> buildWebs(const std::vector<LiveRange>& ranges) {
     return webs;
 }
 
+bool sameLine(const ProgramPoint& a, const ProgramPoint& b) {
+    return a.line == b.line;
+}
+
+bool intersects(const LiveRange& a, const LiveRange& b) {
+    for (const ProgramPoint& p1 : a.points) {
+        for (const ProgramPoint &p2: b.points) {
+            if (sameLine(p1, p2)) {
+                return true;
+            }
+
+        }
+    }
+    return false;
+}
 
 bool intersects(const Web& web, const LiveRange& range) {
-    return true;
+    for (const ProgramPoint& p1 : web.points) {
+        for (const ProgramPoint &p2: range.points) {
+            if (sameLine(p1, p2)) {
+                return true;
+            }
+
+        }
+    }
+    return false;
 }
